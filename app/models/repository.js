c1fe7175f2d6c473b9c8d7babc7e44436cb7164b
Tom Gehrke <phyrog@cognitive-coding.com>
2016-10-04 14:54:36 +0200

    Add belongsTo association

--- app/models/repository.js
+++ app/models/repository.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   slug: DS.attr('string'),
-  description: DS.attr('string')
+  description: DS.attr('string'),
+  namespace: DS.belongsTo()
 });
--- tests/unit/models/repository-test.js
+++ tests/unit/models/repository-test.js
@@ -1,19 +1,25 @@
 import { expect } from 'chai';
 import { describeModel, it } from 'ember-mocha';
+import Ember from 'ember';
 
 describeModel(
   'repository',
   'Unit | Model | repository',
   {
     // Specify the other units that are required for this test.
-    needs: []
+    needs: ['model:namespace']
   },
   function() {
-    // Replace this with your real tests.
     it('has a name', function() {
       let model = this.subject();
-      // var store = this.store();
       expect(model).to.be.ok;
+    });
+
+    it('belongs to a namespace', function() {
+      let model = this.store().modelFor('repository');
+      let relationship = Ember.get(model, 'relationshipsByName').get('namespace');
+      expect(relationship.key).to.equal('namespace');
+      expect(relationship.kind).to.equal('belongsTo');
     });
   }
 );
