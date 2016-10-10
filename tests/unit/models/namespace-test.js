80d7bd3212895caa20961ca662eb4a845ebdaa8b
Tom Gehrke <phyrog@cognitive-coding.com>
2016-10-10 08:54:02 +0200

    Add eslint ignore line to test

--- tests/unit/models/namespace-test.js
+++ tests/unit/models/namespace-test.js
@@ -4,6 +4,7 @@ import { describeModel, it } from 'ember-mocha';
 describeModel(
   'namespace',
   'Unit | Model | namespace',
+  // eslint-disable-next-line prefer-arrow-callback
   function() {
     it('has a name', function() {
       let model = this.subject({ name: 'Foobar' });
