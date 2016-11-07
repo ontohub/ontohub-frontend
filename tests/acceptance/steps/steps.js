adfddb760a368aac494384803921c93da0a09e80
Tom Gehrke <phyrog@cognitive-coding.com>
2016-11-07 10:16:47 +0100

    Write index page tests

--- tests/acceptance/steps/steps.js
+++ tests/acceptance/steps/steps.js
@@ -1,7 +1,13 @@
+/* eslint-disable no-undef */
+
 import yadda from '../../helpers/yadda';
 
 export default function() {
   return yadda.localisation.English.library()
+    .given('I am not logged in', (next) => {
+      // TODO: Fill step when authentication is in place
+      andThen(() => next());
+    })
     .given('I type "Ember g feature make-feature"', (next) => {
       visit('/');
       // Add your own assert library
