d21008da38fc5e1fe12289430527bac8257fa16d
Eileen Bolloff <ebolloff@informatik.uni-bremen.de>
2016-11-18 18:25:24 +0100

    Add user name in repository new select.

--- app/routes/namespace/repository/new.js
+++ app/routes/namespace/repository/new.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return this.store.findRecord('namespace', 'ada');
+  },
   renderTemplate() {
     this.render();
     this.render('namespace/repository/new-header', {
