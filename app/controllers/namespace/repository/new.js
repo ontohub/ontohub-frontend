69f966f6c45a6c95c60b6cc36ee21a6bb62b0623
Eileen Bolloff <ebolloff@informatik.uni-bremen.de>
2016-11-25 15:27:15 +0100

    Add repository controller.

---
+++ app/controllers/namespace/repository/new.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  content_type: "ontology",
+  public_access: true,
+  actions: {
+    submitNewRepo: function() {
+      let model = this.get('model');
+      let repo = model.repository;
+      repo.set('namespace', model.namespace);
+      repo.set('content_type', this.get('content_type'));
+      repo.set('public_access', this.get('public_access'));
+      repo.save().then((repo) => {
+        let _slugs = repo.id.split('/');
+        this.transitionToRoute('namespace.repository.show', _slugs[0], _slugs[1]);
+      });
+      return false;
+    }
+  }
+});
