d93bfc80d95e57df75e5546339d286ca6f3a8176
Tom Gehrke <phyrog@cognitive-coding.com>
2016-11-25 17:14:23 +0100

    Integrate repository routes

--- app/components/search-result.js
+++ app/components/search-result.js
@@ -1,5 +1,21 @@
 import Ember from 'ember';
+import _ from 'lodash';
 
-export default Ember.Component.extend({
-  localClassNames: 'search-result'
+const SearchResultComponent = Ember.Component.extend({
+  localClassNames: 'search-result',
+  icon: Ember.computed('params.[]', function() {
+    return this.get('params')[0];
+  }),
+  title: Ember.computed('params.[]', function() {
+    return this.get('params')[1];
+  }),
+  routeParams: Ember.computed('params.[]', function() {
+    return _.drop(this.get('params'), 2);
+  }),
 });
+
+SearchResultComponent.reopenClass({
+  positionalParams: 'params'
+});
+
+export default SearchResultComponent;
