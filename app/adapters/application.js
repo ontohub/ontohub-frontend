df90b811b6474843c529c8f921a9184d01fe7c79
Eileen Bolloff <ebolloff@informatik.uni-bremen.de>
2017-02-20 15:43:56 +0100

    Rename DataAdapterMixin to SimpleAuthData.

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,10 +1,10 @@
 import Ember from 'ember';
 import DS from 'ember-data';
 import ENV from "../config/environment";
-import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
+import SimpleAuthData from 'ember-simple-auth/mixins/data-adapter-mixin';
 const { underscore, pluralize } = Ember.String;
 
-export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
+export default DS.JSONAPIAdapter.extend(SimpleAuthData, {
   // if your rails app is on a different port from your ember app
   // this can be helpful for development.
   // in production, the host for both rails and ember should be the same.
