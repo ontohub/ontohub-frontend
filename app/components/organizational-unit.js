13db8b5cb417deb744b7d324e88d15444d94ca5c
Tom Gehrke <phyrog@cognitive-coding.com>
2017-02-20 15:25:22 +0100

    Create components

---
+++ app/components/organizational-unit.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  classNames: ['organizational-unit'],
+  isUser: Ember.computed('orgUnit', function() {
+    return this.get('orgUnit').constructor.modelName === 'user';
+  })
+});
