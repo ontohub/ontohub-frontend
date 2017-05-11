800800dd8cf2cb0870276709a07d0793e92a4884
Tom Gehrke <phyrog@cognitive-coding.com>
2017-05-11 13:09:13 +0200

    Replace double quotes by singles

--- testem.js
+++ testem.js
@@ -1,29 +1,29 @@
 /* eslint-env node */
 
 let config = {
-  "framework": "mocha",
-  "test_page": "tests/index.html?hidepassed",
-  "disable_watching": true,
-  "ignore_missing_launchers": true,
-  "parallel": 3,
-  "launch_in_ci": [
-    "PhantomJS"
+  'framework': 'mocha',
+  'test_page': 'tests/index.html?hidepassed',
+  'disable_watching': true,
+  'ignore_missing_launchers': true,
+  'parallel': 3,
+  'launch_in_ci': [
+    'PhantomJS'
   ],
-  "launch_in_dev": [
-    "PhantomJS",
-    "Chromium",
-    "Chrome"
+  'launch_in_dev': [
+    'PhantomJS',
+    'Chromium',
+    'Chrome'
   ],
-  "browser_args": {
-    "Chromium": [
-      "--headless",
-      "--disable-gpu",
-      "--remote-debugging-port=9222"
+  'browser_args': {
+    'Chromium': [
+      '--headless',
+      '--disable-gpu',
+      '--remote-debugging-port=9222'
     ],
-    "Chrome": [
-      "--headless",
-      "--disable-gpu",
-      "--remote-debugging-port=9222"
+    'Chrome': [
+      '--headless',
+      '--disable-gpu',
+      '--remote-debugging-port=9222'
     ]
   }
 }
