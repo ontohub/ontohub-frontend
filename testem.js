36a423875335cf89ed916dd7cebc80b8f62cb41b
Tom Gehrke <phyrog@cognitive-coding.com>
2017-05-11 18:41:47 +0200

    Allow local testem overrides

--- testem.js
+++ testem.js
@@ -1,40 +1,46 @@
 /* eslint-env node */
 
+/*
+ * Default testem settings. You can locally override these by creating a file
+ * `testem.local.js`. Example:
+ * ```
+ * module.exports = (defaults) => ({
+ *   launch_in_ci: defaults.launch_in_ci.concat(['Chrome', 'Safari']),
+ *   parallel: 3
+ * })
+ * ```
+ */
+
 let config = {
-  'framework': 'mocha',
-  'test_page': 'tests/index.html?hidepassed',
-  'disable_watching': true,
-  'ignore_missing_launchers': true,
-  'parallel': 3,
-  'launch_in_ci': [
-    'PhantomJS'
-  ],
-  'launch_in_dev': [
-    'PhantomJS',
-    'Chromium',
-    'Chrome'
-  ],
-  'browser_args': {
-    'Chromium': [
-      '--headless',
-      '--disable-gpu',
-      '--remote-debugging-port=9222'
-    ],
-    'Chrome': [
-      '--headless',
-      '--disable-gpu',
-      '--remote-debugging-port=9222'
-    ]
+  framework: 'mocha',
+  test_page: 'tests/index.html?hidepassed',
+  disable_watching: true,
+  launch_in_ci: ['PhantomJS'],
+  launch_in_dev: ['PhantomJS', 'Chromium', 'Chrome'],
+  browser_args: {
+    Chromium: ['--headless', '--disable-gpu', '--remote-debugging-port=9222'],
+    Chrome: ['--headless', '--disable-gpu', '--remote-debugging-port=9222']
   }
 }
 
 if (process.env.TRAVIS) {
-  config.browser_args.Chromium.push('--no-sandbox')
-  config.browser_args.Chrome.push('--no-sandbox')
+  Object.assign(
+    config,
+    ((defaults) => ({
+      parallel: 2,
+      launch_in_ci: ['PhantomJS', 'Chromium'],
+      browser_args: {
+        Chromium: defaults.browser_args.Chromium.concat(['--no-sandbox'])
+      }
+    }))(config)
+  )
 }
 
-if (process.env.LAUNCHERS) {
-  config.launch_in_ci = process.env.LAUNCHERS.split(' ')
-}
+try {
+  let localConf = require('./testem.local')
 
+  Object.assign(config, localConf(config))
+} catch (e) {
+  // No local overrides
+}
 module.exports = config
