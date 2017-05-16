f33710c84cae03b032d8f12b12ed8c1b17e72d1f
Tom Gehrke <phyrog@users.noreply.github.com>
2017-05-16 20:54:53 +0200

    Make testem config configurable (#84)
    * Ignore missing testem launchers
    
    * Try to get chrome working on travis
    
    * Disable sandbox on travis
    
    * Make test launchers run in parallel
    
    * Make launchers choosable during test launch
    
    * Add .env file to gitignore
    
    * Replace double quotes by singles
    
    * Allow local testem overrides

--- testem.js
+++ testem.js
@@ -1,27 +1,46 @@
 /* eslint-env node */
 
-module.exports = {
-  "framework": "mocha",
-  "test_page": "tests/index.html?hidepassed",
-  "disable_watching": true,
-  "launch_in_ci": [
-    "PhantomJS"
-  ],
-  "launch_in_dev": [
-    "PhantomJS",
-    "Chromium",
-    "Chrome"
-  ],
-  "browser_args": {
-    "Chromium": [
-      "--headless",
-      "--disable-gpu",
-      "--remote-debugging-port=9222"
-    ],
-    "Chrome": [
-      "--headless",
-      "--disable-gpu",
-      "--remote-debugging-port=9222"
-    ]
+/*
+ * Default testem settings. You can locally override these by creating a file
+ * `testem.local.js`. Example:
+ * ```
+ * module.exports = (defaults) => ({
+ *   launch_in_ci: defaults.launch_in_ci.concat(['Chrome', 'Safari']),
+ *   parallel: 3
+ * })
+ * ```
+ */
+
+let config = {
+  framework: 'mocha',
+  test_page: 'tests/index.html?hidepassed',
+  disable_watching: true,
+  launch_in_ci: ['PhantomJS'],
+  launch_in_dev: ['PhantomJS', 'Chromium', 'Chrome'],
+  browser_args: {
+    Chromium: ['--headless', '--disable-gpu', '--remote-debugging-port=9222'],
+    Chrome: ['--headless', '--disable-gpu', '--remote-debugging-port=9222']
   }
 }
+
+if (process.env.TRAVIS) {
+  Object.assign(
+    config,
+    ((defaults) => ({
+      parallel: 2,
+      launch_in_ci: ['PhantomJS', 'Chromium'],
+      browser_args: {
+        Chromium: defaults.browser_args.Chromium.concat(['--no-sandbox'])
+      }
+    }))(config)
+  )
+}
+
+try {
+  let localConf = require('./testem.local')
+
+  Object.assign(config, localConf(config))
+} catch (e) {
+  // No local overrides
+}
+module.exports = config
