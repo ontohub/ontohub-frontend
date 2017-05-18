f45b2d1d5893bb77201228fda1860cc7159266ed
Eugen Kuksa <kuksa.eugen@gmail.com>
2017-05-18 09:14:46 +0200

    Unify user name and id.

--- app/adapters/user.js
+++ app/adapters/user.js
@@ -1,17 +1,10 @@
-import ApplicationAdapter from './application';
+import ApplicationAdapter from './application'
 
 export default ApplicationAdapter.extend({
   urlForCreateRecord() {
-    return `${this.get('host')}/users`;
+    return `${this.get('host')}/users`
   },
   urlForDeleteRecord() {
-    return `${this.get('host')}/users`;
-  },
-  urlForQueryRecord(query, modelName) {
-    if (typeof(query.filter.name) == 'string') {
-      return `${this.get('host')}/users/by-name/${query.filter.name}`
-    } else {
-      super.urlForQueryRecord(query, modelName)
-    }
+    return `${this.get('host')}/users`
   }
-});
+})
--- app/models/organizational-unit.js
+++ app/models/organizational-unit.js
@@ -1,5 +1,8 @@
 import DS from 'ember-data'
 
 export default DS.Model.extend({
+  // The name is not part of the JSON schema, but it is needed for registration
+  // and signing in.
+  name: DS.attr('string'),
   repositories: DS.hasMany('repository')
 })
