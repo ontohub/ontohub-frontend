04833e7a125c4964104b2acceedd90fedefa5484
Tom Gehrke <phyrog@cognitive-coding.com>
2017-05-30 22:58:00 +0200

    Add VersionWarning container test

---
+++ src/containers/__tests__/VersionWarning.test.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import VersionWarning from '../VersionWarning'
+import { ApolloProvider } from 'react-apollo'
+import { client } from '../../apollo'
+import { mount } from 'enzyme'
+
+describe('VersionWarning Container', () => {
+  let requirement = '> v0.0.0-65'
+
+  it('calls the API', () => {
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        json() {
+          return Promise.resolve({
+            data: {
+              attributes: {
+                tag: '0.0.0',
+                commits_since_tag: 80
+              }
+            }
+          })
+        }
+      })
+    )
+    mount(
+      <ApolloProvider client={client}>
+        <VersionWarning requirement={requirement} />
+      </ApolloProvider>
+    )
+    expect(fetch.mock.calls.length).toBe(1)
+  })
+})
