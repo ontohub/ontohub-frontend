7088bbf7dc9cad434087431c879adb5d89bd1395
Tom Gehrke <phyrog@cognitive-coding.com>
2017-05-30 15:30:59 +0200

    Add react router

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
+import { BrowserRouter as Router } from 'react-router-dom'
 import './index.css'
 import 'semantic-ui-css/semantic.min.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById('root')
+)
 registerServiceWorker()
