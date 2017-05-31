8922776dc2514e6bbd8d18a6bc2a5ee39db5313f
Tom Gehrke <phyrog@cognitive-coding.com>
2017-05-31 10:09:21 +0200

    Exclude files src/*.js from coverage

--- src/__tests__/App.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from '../App'
-
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  // ReactDOM.render(<App config={{ version: '> v0.0.0-65' }} />, div)
-})
