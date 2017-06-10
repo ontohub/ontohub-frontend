b8c0342e2557739c1d07a0d51d52047777795370
Tom Gehrke <phyrog@cognitive-coding.com>
2017-06-10 16:24:36 +0200

    Fix flow checks

--- src/App.js
+++ src/App.js
@@ -1,44 +1,39 @@
 // @flow
 
-import React, { Component } from 'react'
+import React from 'react'
 import { Header } from './components'
 import { VersionWarning, GlobalMenu } from './containers'
 import { Switch, Route } from 'react-router-dom'
 import styled from 'styled-components'
 import routes from './routes'
 
-class App extends Component {
-  render() {
-    return (
-      <div className={this.props.className}>
-        <GlobalMenu />
-        <Header>
-          <Switch>
-            {routes.map((route, index) =>
-              <Route
-                key={index}
-                path={route.path}
-                exact={route.exact}
-                component={route.header}
-              />
-            )}
-          </Switch>
-        </Header>
-        <Switch>
-          {routes.map((route, index) =>
-            <Route
-              key={index}
-              path={route.path}
-              exact={route.exact}
-              component={route.main}
-            />
-          )}
-        </Switch>
-        <VersionWarning requirement={this.props.config.version} />
-      </div>
-    )
-  }
-}
+const App = (props) =>
+  <div className={props.className}>
+    <GlobalMenu />
+    <Header>
+      <Switch>
+        {routes.map((route, index) =>
+          <Route
+            key={index}
+            path={route.path}
+            exact={route.exact}
+            component={route.header}
+          />
+        )}
+      </Switch>
+    </Header>
+    <Switch>
+      {routes.map((route, index) =>
+        <Route
+          key={index}
+          path={route.path}
+          exact={route.exact}
+          component={route.main}
+        />
+      )}
+    </Switch>
+    <VersionWarning requirement={props.config.version} />
+  </div>
 
 const StyledApp = styled(App)`
   position: relative;
