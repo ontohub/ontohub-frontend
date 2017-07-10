3861f29773484ee2deb133d686d666aa4216139c
Tom Gehrke <phyrog@cognitive-coding.com>
2017-07-10 11:41:11 +0200

    Add accept json header to requests

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -9,11 +9,26 @@ networkInterface.use([
   {
     applyMiddleware(req, next) {
       if (!req.options.headers) {
-        req.options.headers = {} // Create the header object if needed.
+        req.options.headers = {}
       }
+      next()
+    }
+  },
+  {
+    applyMiddleware(req, next) {
+      Object.assign(req.options.headers, {
+        Accept: 'application/json'
+      })
+      next()
+    }
+  },
+  {
+    applyMiddleware(req, next) {
       let authToken = localStorage.getItem('auth-token')
       if (authToken) {
-        req.options.headers['Authorization'] = `Bearer ${authToken}`
+        Object.assign(req.options.headers, {
+          Authorization: `Bearer ${authToken}`
+        })
       }
       next()
     }
