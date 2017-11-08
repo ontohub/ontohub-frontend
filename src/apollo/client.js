7d8f038b474f8f39d0995c5f6fb8d2bff2b7aaa9
Tom Gehrke <phyrog@cognitive-coding.com>
2017-11-08 09:48:57 +0100

    Split links

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -1,38 +1,6 @@
 import { ApolloClient } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
-import { BatchHttpLink } from "apollo-link-batch-http";
-import { onError } from "apollo-link-error";
-import _ from "lodash";
-import { signOut } from "../helpers/session";
-
-const backendHost = process.env.REACT_APP_BACKEND_HOST;
-
-const networkInterface = createBatchingNetworkInterface({
-  uri: `${backendHost}/graphql`,
-  batchInterval: 10
-});
-
-const batchHttpLink = new BacthHttpLink({
-  uri: `${backendHost}/graphql`
-});
-
-const headerLink = new ApolloLink((operation, forward) => {
-  const authToken = localStorage.getItem("auth-token");
-  operation.setContext({
-    headers: {
-      authorization: `Bearer ${authToken}`,
-      accept: "application/json"
-    }
-  });
-  return forward(operation);
-});
-
-const errorLink = onError(({ graphQLErrors, networkError }) => {
-  console.log(graphQLErrors);
-  console.log(networkError);
-});
-
-const link = errorLink.concat(headerLink.concat(batchHttpLink));
+import link from "./links";
 
 export const Client = new ApolloClient({
   link,
