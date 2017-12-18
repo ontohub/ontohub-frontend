480c3cac1ab8704bea269d03e3dd3c4214bd52e8
Tom Gehrke <phyrog@cognitive-coding.com>
2017-12-18 10:05:54 +0100

    Add jest-styled-components

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,7 @@
 import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import serializer from "enzyme-to-json/serializer";
+import "jest-styled-components";
 
 configure({ adapter: new Adapter() });
 
