5bddbb3018c2f3ca4cfcd4fdca1ce5a4f1b1fbe6
Tom Gehrke <phyrog@cognitive-coding.com>
2017-12-20 17:04:52 +0100

    Move setServerErrors tests

--- src/components/SignUpForm/__tests__/validation.test.js
+++ src/components/SignUpForm/__tests__/validation.test.js
@@ -1,7 +1,6 @@
 import {
   addError,
   formHasError,
-  setServerErrors,
   validate,
   isUsernameAvailable
 } from "../validation";
@@ -41,48 +40,6 @@ describe("formHasError", () => {
   it("returns false if none of the relevant fields has an error", () => {
     let errors = { otherField: ["This is an error"] };
     expect(formHasError(errors)).toBeFalsy();
-  });
-});
-
-describe("setServerErrors", () => {
-  let setErrors;
-  let setSubmitting;
-  let onError;
-
-  beforeEach(() => {
-    setErrors = jest.fn();
-    setSubmitting = jest.fn();
-    onError = jest.fn();
-    setServerErrors(setErrors, setSubmitting, onError)({
-      message: `
-        GraphQL error: Username not found
-        GraphQL error: Password is missing
-      `
-    });
-  });
-  it("calls setErrors with the found errors", () => {
-    expect(setErrors.mock.calls).toMatchSnapshot();
-  });
-  it("calls setSubmitting", () => {
-    expect(setSubmitting.mock.calls).toEqual([[false]]);
-  });
-  it("calls onError", () => {
-    expect(onError.mock.calls.length).toEqual(1);
-  });
-
-  describe("with an invalid error message", () => {
-    beforeEach(() => {
-      setErrors = jest.fn();
-      setSubmitting = jest.fn();
-      onError = jest.fn();
-      setServerErrors(setErrors, setSubmitting, onError)({
-        message: "This is an invalid error message"
-      });
-    });
-
-    it("calls setErrors with the found errors", () => {
-      expect(setErrors.mock.calls).toMatchSnapshot();
-    });
   });
 });
 
