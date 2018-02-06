87672c7dca3363faa5ef3b50f8c3fe9b69ad0c18
Tom Gehrke <phyrog@cognitive-coding.com>
2018-02-06 22:16:17 +0100

    Write some more tests

---
+++ src/components/__tests__/HeadAndBody.test.js
@@ -0,0 +1,21 @@
+import { HeadAndBody } from "../HeadAndBody";
+import { ThemeProvider } from "styled-components";
+
+describe("HeadAndBody", () => {
+  set("component", () => (
+    <ThemeProvider
+      theme={{ colors: { dark: "#000" }, sizes: { contentWidth: "700px" } }}
+    >
+      <HeadAndBody
+        head={() => <h1>Head</h1>}
+        body={() => <div>Body</div>}
+        hoc={Component => props => <Component {...props} className="HOCed" />}
+      />
+    </ThemeProvider>
+  ));
+  set("renderedComponent", () => render(component));
+
+  it("applies the HOC to head and body", () => {
+    expect(renderedComponent).toMatchSnapshot();
+  });
+});
