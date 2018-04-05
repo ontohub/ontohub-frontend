91a7858a360aa8ac8102ae00f2c14ac5887b5081
Tom Gehrke <phyrog@cognitive-coding.com>
2018-04-05 14:56:11 +0200

    Move App.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import registerServiceWorker from "./registerServiceWorker";
-import App from "./App";
+import App from "./app";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ApolloProvider } from "react-apollo";
 import { Client as client } from "config/apollo";
