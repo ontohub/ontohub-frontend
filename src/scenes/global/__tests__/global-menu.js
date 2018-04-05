a656c67cb68c00cf01485d61f948805af474cc06
Tom Gehrke <phyrog@cognitive-coding.com>
2018-04-05 23:07:29 +0200

    Refactor global menu test

--- src/scenes/global/__tests__/global-menu.js
+++ src/scenes/global/__tests__/global-menu.js
@@ -5,40 +5,35 @@ import { ThemeProvider } from "styled-components";
 import theme from "config/theme";
 
 describe("GlobalMenu", () => {
-  describe("signed out user", () => {
-    it("matches the snapshot", () => {
-      const wrapper = render(
-        <Router>
-          <ThemeProvider theme={theme}>
-            <GlobalMenu />
-          </ThemeProvider>
-        </Router>
-      );
-      expect(wrapper).toMatchSnapshot();
-    });
-  });
-  describe("signed in user", () => {
-    it("matches the snapshot", () => {
-      const wrapper = render(
-        <Router>
-          <ThemeProvider theme={theme}>
-            <GlobalMenu
-              me={{ id: "ada", emailHash: "3e3417d7ef77d5932a6734b916515ed5" }}
-            />
-          </ThemeProvider>
-        </Router>
-      );
-      expect(wrapper).toMatchSnapshot();
-    });
-  });
-  it("contains the title link", () => {
-    const wrapper = render(
+  set("me", () => undefined);
+  set("component", () =>
+    render(
       <Router>
         <ThemeProvider theme={theme}>
-          <GlobalMenu />
+          <GlobalMenu me={me} />
         </ThemeProvider>
       </Router>
-    );
-    expect(wrapper.find("a").text()).toContain("Ontohub");
+    )
+  );
+
+  describe("signed out user", () => {
+    it("matches the snapshot", () => {
+      expect(component).toMatchSnapshot();
+    });
+  });
+
+  describe("signed in user", () => {
+    set("me", () => ({
+      id: "ada",
+      emailHash: "3e3417d7ef77d5932a6734b916515ed5"
+    }));
+
+    it("matches the snapshot", () => {
+      expect(component).toMatchSnapshot();
+    });
+  });
+
+  it("contains the title link", () => {
+    expect(component.find("a").text()).toContain("Ontohub");
   });
 });
